fcdb5e6c10a44c0fe701a89ebc072e3562147f02
Yash Raj Gupta <yashraj.guptadev@gmail.com>
2023-08-10 15:03:58 +0530

    work till 9/8/23

--- frontend/loginpage/script.js
+++ frontend/loginpage/script.js
@@ -1,53 +1,49 @@
 // navigation bar
 const toggleMenu = () => {
-    const burgerMenu = document.querySelector(".menu-icon");
-    const src= burgerMenu.getAttribute('src');
-    const iconName = src === '../images/menu.svg'?
-    '../images/close.svg'
-    :
-    '../images/menu.svg';
+  const burgerMenu = document.querySelector(".menu-icon");
+  const src = burgerMenu.getAttribute("src");
+  const iconName =
+    src === "../images/menu.svg" ? "../images/close.svg" : "../images/menu.svg";
 
-    burgerMenu.setAttribute('src',iconName );
-    const navigation = document.querySelector('.navigation');
-    navigation.classList.toggle (
-        'navigation--mobile'
-    );
+  burgerMenu.setAttribute("src", iconName);
+  const navigation = document.querySelector(".navigation");
+  navigation.classList.toggle("navigation--mobile");
 };
 //nav ends
 
 //login/signup box
 const container = document.querySelector(".container"),
-      pwShowHide = document.querySelectorAll(".showHidePw"),
-      pwFields = document.querySelectorAll(".password"),
-      signUp = document.querySelector(".signup-link"),
-      login = document.querySelector(".login-link");
+  pwShowHide = document.querySelectorAll(".showHidePw"),
+  pwFields = document.querySelectorAll(".password"),
+  signUp = document.querySelector(".signup-link"),
+  login = document.querySelector(".login-link");
 
-    //   js code to show/hide password and change icon
-    pwShowHide.forEach(eyeIcon =>{
-        eyeIcon.addEventListener("click", ()=>{
-            pwFields.forEach(pwField =>{
-                if(pwField.type ==="password"){
-                    pwField.type = "text";
+//   js code to show/hide password and change icon
+pwShowHide.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    pwFields.forEach((pwField) => {
+      if (pwField.type === "password") {
+        pwField.type = "text";
 
-                    pwShowHide.forEach(icon =>{
-                        icon.classList.replace("uil-eye-slash", "uil-eye");
-                    })
-                }else{
-                    pwField.type = "password";
+        pwShowHide.forEach((icon) => {
+          icon.classList.replace("uil-eye-slash", "uil-eye");
+        });
+      } else {
+        pwField.type = "password";
 
-                    pwShowHide.forEach(icon =>{
-                        icon.classList.replace("uil-eye", "uil-eye-slash");
-                    })
-                }
-            }) 
-        })
-    })
-
-    // js code to appear signup and login form
-    signUp.addEventListener("click", ( )=>{
-        container.classList.add("active");
+        pwShowHide.forEach((icon) => {
+          icon.classList.replace("uil-eye", "uil-eye-slash");
+        });
+      }
     });
-    login.addEventListener("click", ( )=>{
-        container.classList.remove("active");
-    });
+  });
+});
+
+// js code to appear signup and login form
+signUp.addEventListener("click", () => {
+  container.classList.add("active");
+});
+login.addEventListener("click", () => {
+  container.classList.remove("active");
+});
 //login/signup box
--- frontend/script.js
+++ frontend/script.js
@@ -1,140 +1,136 @@
 // navigation bar
 const toggleMenu = () => {
-    const burgerMenu = document.querySelector(".menu-icon");
-    const src= burgerMenu.getAttribute('src');
-    const iconName = src === 'images/menu.svg'?
-    'images/close.svg'
-    :
-    'images/menu.svg';
+  const burgerMenu = document.querySelector(".menu-icon");
+  const src = burgerMenu.getAttribute("src");
+  const iconName =
+    src === "images/menu.svg" ? "images/close.svg" : "images/menu.svg";
 
-    burgerMenu.setAttribute('src',iconName );
-    const navigation = document.querySelector('.navigation');
-    navigation.classList.toggle (
-        'navigation--mobile'
-    );
+  burgerMenu.setAttribute("src", iconName);
+  const navigation = document.querySelector(".navigation");
+  navigation.classList.toggle("navigation--mobile");
 };
 
 //Progress bar
 const body = document.body;
-const progressBar = document.querySelector('.progress__bar');
-console.log(progressBar)
+const progressBar = document.querySelector(".progress__bar");
+console.log(progressBar);
 const updateProgress = () => {
-    let scrollPos =
-        (window.scrollY / (body.scrollHeight - window.innerHeight)) * 100;
-    console.log(scrollPos)
-    progressBar.style.width = `${scrollPos}%`;
-    requestAnimationFrame(updateProgress);
+  let scrollPos =
+    (window.scrollY / (body.scrollHeight - window.innerHeight)) * 100;
+  console.log(scrollPos);
+  progressBar.style.width = `${scrollPos}%`;
+  requestAnimationFrame(updateProgress);
 };
 
-updateProgress()
+updateProgress();
 
 // faqs
 class Accordion {
-    constructor(el) {
-      this.el = el;
-      this.summary = el.querySelector('summary');
-      this.content = el.querySelector('.faq-content');
-      this.expandIcon = this.summary.querySelector('.expand-icon')
-      this.animation = null;
-      this.isClosing = false;
-      this.isExpanding = false;
-      this.summary.addEventListener('click', (e) => this.onClick(e));
-    }
-  
-    onClick(e) {
-      e.preventDefault();
-      this.el.style.overflow = 'hidden';
+  constructor(el) {
+    this.el = el;
+    this.summary = el.querySelector("summary");
+    this.content = el.querySelector(".faq-content");
+    this.expandIcon = this.summary.querySelector(".expand-icon");
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.summary.addEventListener("click", (e) => this.onClick(e));
+  }
 
-      if (this.isClosing || !this.el.open) {
-        this.open();
-      } else if (this.isExpanding || this.el.open) {
-        this.shrink();
-      }
-    }
-  
-    shrink() {
-      this.isClosing = true;
+  onClick(e) {
+    e.preventDefault();
+    this.el.style.overflow = "hidden";
 
-      const startHeight = `${this.el.offsetHeight}px`;
-      const endHeight = `${this.summary.offsetHeight}px`;
-
-      if (this.animation) {
-        this.animation.cancel();
-      }
-      
-      this.animation = this.el.animate({
-        height: [startHeight, endHeight]
-      }, {
-        duration: 400,
-        easing: 'ease-out'
-      });
-
-      this.animation.onfinish = () => {
-        this.expandIcon.setAttribute('src', 'images/plus.svg');
-        return this.onAnimationFinish(false);
-      }
-      this.animation.oncancel = () => {
-        this.expandIcon.setAttribute('src', 'images/plus.svg');
-        return this.isClosing = false;
-      }
-    }
-  
-    open() {
-      this.el.style.height = `${this.el.offsetHeight}px`;
-      this.el.open = true;
-      window.requestAnimationFrame(() => this.expand());
-    }
-  
-    expand() {
-      this.isExpanding = true;
-
-      const startHeight = `${this.el.offsetHeight}px`;
-      const endHeight = `${this.summary.offsetHeight + 
-                           this.content.offsetHeight}px`;
-
-      if (this.animation) {
-        this.animation.cancel();
-      }
-      
-      this.animation = this.el.animate({
-        height: [startHeight, endHeight]
-      }, {
-        duration: 350,
-        easing: 'ease-out'
-      });
-
-      this.animation.onfinish = () => {
-        this.expandIcon.setAttribute(
-            'src',
-            'images/minus.svg'
-        );
-        return this.onAnimationFinish(true);
-      }
-      this.animation.oncancel = () => {
-        this.expandIcon.setAttribute(
-            'src',
-            'images/minus.svg'
-        );
-        return this.isExpanding = false;
-      }
-    }
-  
-    onAnimationFinish(open) {
-      this.el.open = open;
-      this.animation = null;
-      this.isClosing = false;
-      this.isExpanding = false;
-      this.el.style.height = this.el.style.overflow = '';
+    if (this.isClosing || !this.el.open) {
+      this.open();
+    } else if (this.isExpanding || this.el.open) {
+      this.shrink();
     }
   }
-  
-  document.querySelectorAll('details').forEach((el) => {
-    new Accordion(el);
-  });
+
+  shrink() {
+    this.isClosing = true;
+
+    const startHeight = `${this.el.offsetHeight}px`;
+    const endHeight = `${this.summary.offsetHeight}px`;
+
+    if (this.animation) {
+      this.animation.cancel();
+    }
+
+    this.animation = this.el.animate(
+      {
+        height: [startHeight, endHeight],
+      },
+      {
+        duration: 400,
+        easing: "ease-out",
+      }
+    );
+
+    this.animation.onfinish = () => {
+      this.expandIcon.setAttribute("src", "images/plus.svg");
+      return this.onAnimationFinish(false);
+    };
+    this.animation.oncancel = () => {
+      this.expandIcon.setAttribute("src", "images/plus.svg");
+      return (this.isClosing = false);
+    };
+  }
+
+  open() {
+    this.el.style.height = `${this.el.offsetHeight}px`;
+    this.el.open = true;
+    window.requestAnimationFrame(() => this.expand());
+  }
+
+  expand() {
+    this.isExpanding = true;
+
+    const startHeight = `${this.el.offsetHeight}px`;
+    const endHeight = `${
+      this.summary.offsetHeight + this.content.offsetHeight
+    }px`;
+
+    if (this.animation) {
+      this.animation.cancel();
+    }
+
+    this.animation = this.el.animate(
+      {
+        height: [startHeight, endHeight],
+      },
+      {
+        duration: 350,
+        easing: "ease-out",
+      }
+    );
+
+    this.animation.onfinish = () => {
+      this.expandIcon.setAttribute("src", "images/minus.svg");
+      return this.onAnimationFinish(true);
+    };
+    this.animation.oncancel = () => {
+      this.expandIcon.setAttribute("src", "images/minus.svg");
+      return (this.isExpanding = false);
+    };
+  }
+
+  onAnimationFinish(open) {
+    this.el.open = open;
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.el.style.height = this.el.style.overflow = "";
+  }
+}
+
+document.querySelectorAll("details").forEach((el) => {
+  new Accordion(el);
+});
 
 // footer
 document.getElementById("year").innerHTML = new Date().getFullYear();
-
 
 //contact us
 feather.replace();
